db008c44159b724b85586598ee80433e916a3383
Otavio Fernando <otaviofernando108@gmail.com>
2026-05-27 21:14:54 -0300

    aula 11 implementacoes

--- js/main.js
+++ js/main.js
@@ -46,4 +46,15 @@
       ticking = false;
     });
   });
+
+  /* Detecção de navegação por teclado para acessibilidade */
+  document.body.addEventListener("keydown", function (e) {
+    if (e.key === "Tab") {
+      document.body.classList.add("using-keyboard");
+    }
+  });
+
+  document.body.addEventListener("mousedown", function () {
+    document.body.classList.remove("using-keyboard");
+  });
 })();
